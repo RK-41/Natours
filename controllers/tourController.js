80c0d63bdf4098962f949a538dfe23c24a9117cd
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-25 22:27:46 +0530

    CRUD Implementation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,16 +2,154 @@
 
 TOUR ROUTE CONTROLLER/HANDLER
 
+*/
 
-Validating the Tour ID (Old Code; Replaced with middleware 'checkID')
+// Requiring Core Modules
+const Tour = require('./../models/tourModel');
+
+
+// ROUTE HANDLER FUNCTIONS for TOUR
+// Handler Funtion to get all tours
+exports.getAllTours = async (req, res) => {
+
+   try {
+      // Getting all the Documents from the DB
+      const tours = await Tour.find();
+
+      res.status(200).json({
+         status: 'success',
+         results: tours.length,
+         data: {
+            tours
+         }
+      });
+   } catch (err) {
+      res.status(404).json({
+         status: 'fail',
+         message: err
+      });
+   }
+
+};
+
+// Handler Function to get a specific tour
+exports.getTour = async (req, res) => {
+   try {
+      // Getting a specific tour by using the 'id' parameter/variable
+      const tour = await Tour.findById(req.params.id);
+
+      // Sending back response
+      res.status(200).json({
+         status: 'success',
+         data: {
+            tour
+         }
+      })
+   } catch (err) {
+      res.status(404).json({
+         status: 'fail',
+         message: err
+      })
+   }
+
+   // // Finding the tour having same ID as 'id'
+   // const tour = tours.find(el => el.id === id);    // array
+};
+
+// Handler Function to create a new tour
+exports.createTour = async (req, res) => {
+   // console.log(req.body);
+
+   // 25.10.23 Handling Error using try-catch
+   try {
+      // Creating a new Tour by directly calling the 'create()' on the 'Tour'
+      const newTour = await Tour.create(req.body);
+      // 'Tour.create()' will return a promise which will return a result which will be stored in 'newTour' which will be the newly created document
+
+      res.status(201).json({
+         status: 'success',
+         data: {
+            tour: newTour
+         }
+      });
+
+   } catch (err) {
+      res.status(400).json({
+         status: 'fail',
+         message: err
+      })
+   }
+};
+
+// Handler Function to update a tour
+exports.updateTour = async (req, res) => {
+   try {
+      // Updating the tour
+      const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+         new: true,
+         runValidators: true
+      });
+      // 'req.body' has the data to be updated
+      // Third parameter is an option with 'new' set to 'true' to return the updated document instead of the original one
+
+      res.status(200).json({
+         status: 'success',
+         data: {
+            tour
+         }
+      });
+   } catch (err) {
+      res.status(404).json({
+         status: 'fail',
+         message: err
+      });
+   }
+};
+
+// Handler Function to delete a tour
+exports.deleteTour = async (req, res) => {
+   try {
+      await Tour.findByIdAndDelete(req.params.id);
+
+      res.status(204).json({
+         status: 'success',
+         data: null
+      })
+   } catch (err) {
+      res.status(404).json({
+         status: 'fail',
+         message: err
+      })
+   }
+};
+
+/*
+   OLD CODES
+ 
+   // Validating the Tour ID (Old Code; Replaced with middleware 'checkID')
     if (!tour) {
         return res.status(404).json({
             status: 'fail',
             message: 'Invalid ID'
          });
-    }
-
-    // Exporting the 'checkID' Function/Middleware (Old Code < 24.10.)
+      }
+      
+   // Exporting the 'checkBody' Function/Middleware for missing 'name' or 'price'
+      exports.checkBody = (req, res, next) => {
+         const reqBody = req.body;
+      
+         if (!reqBody.name || !reqBody.price) {
+            return res.status(400).json({
+               status: 'fail',
+               message: 'Missing name or price'
+            })
+         }
+      
+         next();
+      }
+ 
+ 
+   // Exporting the 'checkID' Function/Middleware (Old Code < 24.10.)
     exports.checkID = (req, res, next, val) => {
        Handling the case when requested tour doesn't exist
        if (req.params.id * 1 >= tours.length) {
@@ -21,18 +159,18 @@ Validating the Tour ID (Old Code; Replaced with middleware 'checkID')
              status: 'fail',
              message: 'Invalid ID'
           });
-       }
-    
-       console.log(`Tour ID is ${val}.`);
-       next();
-    }
-
-    // Reading Tours Data (Old Code for Testing)
-    const tours = JSON.parse(
-       fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-       );
-
-
+         }
+         
+         console.log(`Tour ID is ${val}.`);
+         next();
+      }
+      
+   // Reading Tours Data (Old Code for Testing)
+      const tours = JSON.parse(
+         fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+         );
+         
+ 
     // Handler Function to Create a New Tour (Old code < 24.10.)
     exports.createTour = (req, res) => {
     // Creating a new id of the new tour by using last tour's id
@@ -58,82 +196,4 @@ Validating the Tour ID (Old Code; Replaced with middleware 'checkID')
        }
     );
    };
-    */
-
-// Requiring Core Modules
-const Tour = require('./../models/tourModel');
-
-// Exporting the 'checkBody' Function/Middleware for missing 'name' or 'price'
-exports.checkBody = (req, res, next) => {
-   const reqBody = req.body;
-
-   if (!reqBody.name || !reqBody.price) {
-      return res.status(400).json({
-         status: 'fail',
-         message: 'Missing name or price'
-      })
-   }
-
-   next();
-}
-
-// ROUTE HANDLER FUNCTIONS for TOUR
-// Handler Funtion to get all tours
-exports.getAllTours = (req, res) => {
-   res.status(200).json({
-      status: 'success',
-      requestedAt: req.requestedAt,
-      // results: tours.length,
-      // data: {
-      //    tours
-      // }
-   })
-};
-
-// Handler Function to get a specific tour
-exports.getTour = (req, res) => {
-   // Converting the id from string to number
-   const id = req.params.id * 1;
-
-   // // Finding the tour having same ID as 'id'
-   // const tour = tours.find(el => el.id === id);    // array
-
-   // // Sending back the response
-   // res.status(200).json({
-   //    status: 'success',
-   //    data: {
-   //       tour
-   //    }
-   // });
-};
-
-// Handler Function to create a new tour
-exports.createTour = (req, res) => {
-   // console.log(req.body);
-
-   res.status(201).json({
-      status: 'success',
-      // data: {
-      //    tour: newTour
-      // }
-   });
-};
-
-// Handler Function to update a tour
-exports.updateTour = (req, res) => {
-   res.status(200).json({
-      status: 'success',
-      data: {
-         tour: '<Updata tour here...>'
-      }
-   })
-};
-
-// Handler Function to delete a tour
-exports.deleteTour = (req, res) => {
-   // 'status(204)' means 'null content'
-   res.status(204).json({
-      status: 'success',
-      data: null
-   })
-};
+*/
